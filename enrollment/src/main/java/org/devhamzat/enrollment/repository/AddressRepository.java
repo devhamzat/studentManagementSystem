927ad456a0475d5b0414641a9d93242cf692d399
devhamzat <ayomidelegbede@gmail.com>
2024-03-27 14:52:27 +0100

    one error fixed

---
+++ enrollment/src/main/java/org/devhamzat/enrollment/repository/AddressRepository.java
@@ -0,0 +1,13 @@
+package org.devhamzat.enrollment.repository;
+
+import org.devhamzat.enrollment.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+    Optional<Address> findByHouseNumberAndStreetAndStateAndAndCountry(Integer integer, String street, String state,String country);
+}
